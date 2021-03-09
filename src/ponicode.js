70f23cdf82d4a33e7e86b083041e3b438852bf46
rahul-ranjan-au8 <rahulranjan98@outlook.com>
2021-03-09 15:57:30 +0530

    added tests

--- src/ponicode.js
+++ src/ponicode.js
@@ -1,34 +1,34 @@
-// // Primitive arguments
-// function calculateCommissionJs(volume) {
-//     var fee, rate, commission, bonus, award;
-//     fee = 0.0;
-//     bonus = 0.0;
-//     if (volume > 200000.0) {
-//       rate = 0.15;
-//       bonus = 1000.0;
-//     } else {
-//       if (volume > 100000.0) rate = 0.123;
-//       else {
-//         if (volume > 50000.0) rate = 0.1;
-//         else {
-//           if (volume >= 10000.0) rate = 0.075;
-//           else {
-//             if (volume >= 5000.0) rate = 0.05;
-//             else {
-//               if (volume > 0.0) rate = 0.025;
-//               else {
-//                 rate = 0;
-//                 fee = 50.0;
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//     commission = Math.trunc(volume * rate * 100) / 100;
-//     award = commission + bonus - fee;
-//     return Math.trunc(award * 100) / 100;
-// }
+// Primitive arguments
+function calculateCommissionJs(volume) {
+  var fee, rate, commission, bonus, award;
+  fee = 0.0;
+  bonus = 0.0;
+  if (volume > 200000.0) {
+    rate = 0.15;
+    bonus = 1000.0;
+  } else {
+    if (volume > 100000.0) rate = 0.123;
+    else {
+      if (volume > 50000.0) rate = 0.1;
+      else {
+        if (volume >= 10000.0) rate = 0.075;
+        else {
+          if (volume >= 5000.0) rate = 0.05;
+          else {
+            if (volume > 0.0) rate = 0.025;
+            else {
+              rate = 0;
+              fee = 50.0;
+            }
+          }
+        }
+      }
+    }
+  }
+  commission = Math.trunc(volume * rate * 100) / 100;
+  award = commission + bonus - fee;
+  return Math.trunc(award * 100) / 100;
+}
 
 // // Structured arguments
 // function getClientAgeGenerationJs(client){
